c880c014ed7fb825986361aed8b1cf62905a2103
asyongasyong123 <asyongasyong123@gmail.com>
2026-06-14 19:42:54 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -e
 PORT="${PORT:-8080}"
-
-# Siguradong maminaw sa 0.0.0.0:$PORT
-sed -i "s|listen 8080 ssl http2;|listen 0.0.0.0:${PORT} ssl http2;|g" /usr/local/openresty/nginx/conf/nginx.conf
-sed -i "s|listen \[::\]:8080 ssl http2;|listen [::]:${PORT} ssl http2;|g" /usr/local/openresty/nginx/conf/nginx.conf
-
+sed -i.bak "s|listen 8080 ssl http2;|listen 0.0.0.0:${PORT} ssl http2;|g" /usr/local/openresty/nginx/conf/nginx.conf
+sed -i.bak "s|listen \[::\]:8080 ssl http2;|listen [::]:${PORT} ssl http2;|g" /usr/local/openresty/nginx/conf/nginx.conf
+rm -f /usr/local/openresty/nginx/conf/nginx.conf.bak
 xray run -c /etc/xray.json &
 exec /usr/local/openresty/bin/openresty -g "daemon off;"
